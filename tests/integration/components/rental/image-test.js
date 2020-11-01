0c3c89a0afb2ad1ca3f908931dbd39048b5f98ce
Oliver <ocben1@gmail.com>
2020-11-02 08:10:25 +1100

    toggle image size function

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,31 +1,45 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function(hooks) {
   setupRenderingTest(hooks);
 
-  // test('it renders', async function(assert) {
-  //   // Set any properties with this.set('myProperty', 'value');
-  //   // Handle any actions with this.set('myAction', function(val) { ... });
-
-  //   await render(hbs`<Rental::Image />`);
-
-  //   assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    test('it renders the given image', async function(assert) {
+  test('it renders the given image', async function(assert) {
     await render(hbs`
       <Rental::Image
-      src="/assets/images/teaching-tomster.png"
-      alt="Teaching Tomster"
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
       />
     `);
 
-    // assert.equal(this.element.textContent.trim(), 'template block text');
     assert.dom('.image').exists();
     assert.dom('.image img').hasAttribute('src', '/assets/images/teaching-tomster.png');
     assert.dom('.image img').hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test('clicking on the component toggles its size', async function(assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    assert.dom('button.image').exists();
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large')
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
   });
 });
