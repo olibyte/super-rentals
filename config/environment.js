272ba632d7ca5fe67b40466805a755b2b86bf558
Oliver <ocben1@gmail.com>
2020-11-03 10:24:38 +1100

    add mapbox api

--- config/environment.js
+++ config/environment.js
@@ -46,6 +46,6 @@ module.exports = function(environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
-  ENV.APP.MAPBOX_ACCESS_TOKEN = '';
+  ENV.MAPBOX_ACCESS_TOKEN = '';
   return ENV;
 };
